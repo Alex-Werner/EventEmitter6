a9811a93c15384fa8f802520fea6d7a62e91196f
Alex WERNER <alex@giraf.world>
2017-02-28 13:14:45 +0100

    Let's build a great beautiful wall of EventEmitter

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # EventEmitter6
-Because, there is a EventEmitter, EventEmitter2, EventEmitter3, EventEmitter4 and EventEmitter5, now let's welcome EventEmitter6 !
+
+**Because there is an EventEmitter, EventEmitter2, EventEmitter3, EventEmitter4, and EventEmitter5.**
+
+That's a hell lot of them, and because there is no setMaxEventEmitterPkg, let's make EventEmitter great again !    
+And we will make listeners pays for it !    
---
+++ index.js
@@ -0,0 +1,8 @@
+const MexicoPeople=require('event-emitter');
+const ChinaChinaChinaChinaChinaChina = require('Event-Emitter');
+const WeNeedSomeGlobalWarming=require('eventemitter3');
+const GreatWall=require('eventemitter2');
+const TinyChildrenAreNotHorses=require('eventemitter4');
+const BrokenCountry = require('./node_modules/eventemitter5/package.json');
+
+module.exports = {MexicoPeople, ChinaChinaChinaChinaChinaChina, WeNeedSomeGlobalWarming,GreatWall,TinyChildrenAreNotHorses,BrokenCountry}
